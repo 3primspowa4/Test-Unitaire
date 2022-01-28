8242f904d50d07a13acf96f74fd61143e8985947
kGUILLOT <killianguillot@orange.fr>
2022-01-28 15:25:33 +0100

    AssertEquals(false,...)=>AssertFalse ou AssertTrue

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -12,7 +12,7 @@ public class TableTest {
         //  QUAND un client est affecté à une table
         table.clientArrive(client);
         //  ALORS cette table n'est plus sur la liste des tables libres du restaurant
-        assertEquals(true, table.occupe);
+        assertTrue( table.occupe);
     }
     @Test
     public void tableLibre(){
@@ -23,7 +23,7 @@ public class TableTest {
         //QUAND la table est libérée
         table.clientPart();
         //ALORS cette table apparaît sur la liste des tables libres du restaurant
-        assertEquals(false, table.occupe);
+        assertFalse(table.occupe);
 
     }
 }
