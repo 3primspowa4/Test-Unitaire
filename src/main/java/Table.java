8fb19b5338b763c42e52303ec247f2c9dc37df6a
kadi <kadijetou.gassama@epsi.fr>
2022-02-01 21:39:18 +0100

    essaie test2

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -2,10 +2,12 @@ public class Table {
     public MaitreHotel maitreHotel = null;
     public boolean occupe=false;
     public boolean epinglee=false;
+    public Serveur serveur = null;
 
     public void affecter(MaitreHotel maitreHotel) {
         this.maitreHotel = maitreHotel;
     }
+
     public void clientArrive(Restaurant restaurant,Client client){
         occupe=true;
         restaurant.tableOccupe(this);
@@ -18,5 +20,9 @@ public class Table {
 
     public void tableNonPayee(Client client, Commande commande){
         epinglee=true;
+    }
+
+    public void affecter(Serveur serveur) {
+        this.serveur = serveur;
     }
 }
