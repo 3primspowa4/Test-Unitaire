3202d6d344597451ca8c51f4aa74caf048edd15a
Billel <billel.emerton@gmail.com>
2022-02-01 17:10:58 +0100

    optimisation de TableTest

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,22 +1,33 @@
+import java.util.ArrayList;
+
 public class Table {
     public MaitreHotel maitreHotel = null;
-    public boolean occupe=false;
+    private boolean occupe=false;
     public boolean epinglee=false;
+    private ArrayList<Client> lesclients = new ArrayList<>();
 
     public void affecter(MaitreHotel maitreHotel) {
         this.maitreHotel = maitreHotel;
     }
-    public void clientArrive(Restaurant restaurant,Client client){
-        occupe=true;
-        restaurant.tableOccupe(this);
 
-    }
-    public void clientPart(Restaurant restaurant){
-        occupe=false;
-        restaurant.tableLibre(this);
+    public void clientPart(){
+        setOccupe(false);
     }
 
     public void tableNonPayee(Client client, Commande commande){
         epinglee=true;
+    }
+
+    public boolean isOccupe() {
+        return occupe;
+    }
+    public void clientArrive(Client client){
+        setOccupe(true);
+        this.lesclients.add(client);
+
+    }
+    public void setOccupe(boolean occupe) {
+        this.occupe = occupe;
+
     }
 }
